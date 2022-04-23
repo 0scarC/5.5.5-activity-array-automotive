698b91b43f5514fdcd039725a36cb4bc1e3c2a1e
Admin <admin@Admins-MacBook-Pro-2.local>
2022-04-22 22:05:18 -0500

    Test 001

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,15 @@
+
+import * as original from './vehicle.js'
+
+class Car extends original.Vehicle {
+    constructor(sedan) {
+        super("car", "Sedan", 1965, "tan", "used")
+    }
+}
+
 //this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+// const VehicleModule = require("./vehicle")
 
 //this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+// let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
+// console.log(v.make)
--- vehicle.js
+++ vehicle.js
@@ -1,91 +1,47 @@
-class Vehicle {
-    constructor(make, model, year, color, mileage) {
-        this.make = make;
+export class Vehicle {
+    constructor(type, model, year, color, condition) {
+        this.type = type;
         this.model = model;
         this.year = year;
         this.color = color;
-        this.passenger = 0;
-        this.speed = 0;
-        this.mileage = mileage;
-        this.started = false;
-        this.numberOfWheels = 0;
+        this.condition = condition;
     }
-
-    start() {
-        if (this.fuel > 0) {
-            return this.started = true;
-            console.log("engine started...!!!");
-        } else {
-            return this.started = false;
-            console.log("engine cannot start...");
+    location() {
+        switch(this.type) {
+            case 'car':
+                console.log(`This needs to go the Miami, FL.`)
+                break
+            case 'truck':
+                console.log(`This needs to go to Charlotte, NC.`)
+                break
+            case 'SUV':
+                console.log(`This needs to go to Chicago, IL.`)
+                break
         }
     }
-    accelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                console.log(this.speed += 1);
-                this.fuel = this.fuel - 1;
-            } else {
-                console.log("out of fuel.");
-                this.stop();
-            }
-        } else {
-            alert("You need to start the engine first.");
-        }
+    inspect() {
+        console.log(`I want to inspect the ${this.year} ${this.model}.`)
     }
-    decelerate() {
-        if (this.started) {
-            if (this.fuel > 0) {
-                if (this.speed > 0) {
-                    console.log(this.speed -= 1);
-                    this.fuel = this.fuel - 1;
-                } else {
-                    console.log(this + " has stopped moving");
-                    this.fuel = this.fuel - 1;
-                }
-            } else {
-                console.log("out of fuel.");
-                this.stop();
-            }
-        } else {
-            alert("You need to start the engine first.");
-        }
+    paint(newColor) {
+        console.log(`Let's change the paint from ${this.color} to ${newColor}`)
     }
-    stop() {
-        this.started = false;
-    }
-
-    drive() {
-        accelerate();
-    }
-    brake() {
-        decelerate();
-    }
-
-    autoPark()
-    {
-
-    }
-
-    autoDrive()
-    {
-      
-    }
-
-    typeOfVehicle(wheels) {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
-            console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
-            console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
-            console.log(this.model + " " + this.make + " is a Bike");
-        } else {
-            console.log("Unknown type of vehicle");
+    price() {
+        if (this.condition === 'new') {
+            console.log(`Not on sale`)
+        } else if (this.condition === 'used') {
+            console.log(`Reduced price`)
         }
     }
 }
+
+const olds = new Vehicle("car", "Oldsmobile", 2007, "maroon", "used")
+console.log(olds)
+olds.location()
+olds.inspect()
+olds.paint("navy blue")
+olds.price()
 
 //This exports things you want to use from this "module", more info in readme
-module.exports = {
-    Vehicle
-}
+// module.exports = {
+//     Vehicle
+// }
