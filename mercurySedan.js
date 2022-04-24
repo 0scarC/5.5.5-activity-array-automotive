dc843f9ee16304675beccdbf57b1522c2813fb5c
Admin <admin@Admins-MacBook-Pro-2.local>
2022-04-24 09:54:41 -0500

    Methods added

--- mercurySedan.js
+++ mercurySedan.js
@@ -1,13 +1,41 @@
 
 import {Vehicle} from './vehicle.js'
 
-console.log(Vehicle('more', 'than', 'a', 'car', 'bro'))
-
 class Car extends Vehicle {
-    constructor(more) {
-        super("car", "Sedan", 1965, "tan", "used")
+    constructor(type, model, year, color, condition) {
+        super(type, model, year, color, condition)
+        this.maximumPassengers = 5;
+        this.passenger = 0;
+        this.numberOfWheels = 4;
+        this.maximumSpeed = 160;
+        this.fuel = 10;
+        this.scheduleService = false;
+    }
+    loadPassenger(num) {
+        let availableRoom
+        if (passenger + num > 0 && passenger + num <= maximumPassengers) {
+            availableRoom = true
+        } else if (passenger + num > maximumPassengers) {
+            availableRoom = false
+            console.log(`The ${this.year} ${this.type} can't hold ${this.passenger + num} passengers.`)
+        } else {
+            console.log('Please enter a number 0 and above.')
+        }
+    }
+    start() {
+        if (fuel > 0) {
+            this.start
+        }
+    }
+    maintenance(mileage) {
+        if (mileage > 30000) {
+            this.scheduleService = true
+        }
     }
 }
+
+const sedan = new Car("car", "Sedan", 1965, "tan", "used")
+console.log(sedan)
 
 //this includes the vehicle class as a module
 // const VehicleModule = require("./vehicle")
